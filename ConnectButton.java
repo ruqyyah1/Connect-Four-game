2b99f2f4f28d26a63ad96f71fc8eb2b816c6f157
ruqyyah nasr <ruqyyahn@gmail.com>
2024-09-11 10:42:02 -0400

    comments

--- ConnectButton.java
+++ ConnectButton.java
@@ -1,16 +1,7 @@
 
 import javafx.scene.control.Button;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- *
- */
 public class ConnectButton extends Button{
     private int row;
     private int column;
